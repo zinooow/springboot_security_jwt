a2050850e226f5ec1c78d809dde6003f199ed077
jinho0547 <jinho0547@naver.com>
2025-01-08 15:18:03 +0900

    init

---
+++ src/main/kotlin/com/study/springboot_security_jwt/SpringbootSecurityJwtApplication.kt
@@ -0,0 +1,11 @@
+package com.study.springboot_security_jwt
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringbootSecurityJwtApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringbootSecurityJwtApplication>(*args)
+}
---
+++ src/test/kotlin/com/study/springboot_security_jwt/SpringbootSecurityJwtApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.study.springboot_security_jwt
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringbootSecurityJwtApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
